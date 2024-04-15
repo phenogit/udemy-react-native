98913d79b584be9803dc1dcdb1cdd2a6ef5beb7d
John Huang <pheno_the_best@yahoo.com.tw>
2024-04-16 01:04:24 +0800

    section 17

--- react-native-introduction/App.js
+++ react-native-introduction/App.js
@@ -1,12 +1,17 @@
-import { Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { Profile } from "./components/Profile";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { style } from "./App.style";
 
 export default function App() {
   return (
-    <>
-      <Text>Hello, World!</Text>
-      <Text>My first React Native app!</Text>
-      <Profile />
-    </>
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <Text style={style.title}>Hello, World!</Text>
+        <View style={style.square}>
+          <Text style={style.innerText}>Super</Text>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
