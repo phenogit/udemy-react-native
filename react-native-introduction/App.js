4f42a4be54b1636154154a4826f3ef81c48c1f0a
John Huang <pheno_the_best@yahoo.com.tw>
2024-04-16 00:50:57 +0800

    section 15

--- react-native-introduction/App.js
+++ react-native-introduction/App.js
@@ -1,1 +1,12 @@
-export default function App() {}
+import { Text } from "react-native";
+import { Profile } from "./components/Profile";
+
+export default function App() {
+  return (
+    <>
+      <Text>Hello, World!</Text>
+      <Text>My first React Native app!</Text>
+      <Profile />
+    </>
+  );
+}
---
+++ react-native-introduction/components/Profile.jsx
@@ -0,0 +1,12 @@
+import { TouchableOpacity, Text } from "react-native";
+
+export function Profile() {
+  return (
+    <>
+      <Text>Hello I am Pheno</Text>
+      <TouchableOpacity>
+        <Text>Click me!</Text>
+      </TouchableOpacity>
+    </>
+  );
+}
