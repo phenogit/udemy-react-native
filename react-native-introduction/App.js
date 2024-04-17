4b0e8569887a0db1083aa113816c36ca9d2554d0
John Huang <pheno_the_best@yahoo.com.tw>
2024-04-18 02:02:38 +0800

    section 25

--- react-native-introduction/App.js
+++ react-native-introduction/App.js
@@ -1,14 +1,25 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import { Profile, ProfileCard } from "./components/ProfileCard/ProfileCard";
 import { AgeCounter } from "./components/AgeCounter/AgeCounter";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 import { style } from "./App.style";
+import { useState } from "react";
 
 export default function App() {
+  const [movie, setMovie] = useState("The Matrix");
+
+  console.log(movie);
+  function changeMovie() {
+    setMovie("The Matrix Reloaded");
+  }
+
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1, justifyContent: "center", padding: 20 }}>
-        <AgeCounter />
+      <SafeAreaView style={style.container}>
+        <TouchableOpacity onPress={changeMovie}>
+          <Text style={style.text}>{movie}</Text>
+        </TouchableOpacity>
+        {/*<AgeCounter />*/}
         {/*<ProfileCard
           firstName={"Pheno"}
           lastName={"Future"}
--- react-native-introduction/App.style.js
+++ react-native-introduction/App.style.js
@@ -1,6 +1,12 @@
 import { StyleSheet } from "react-native";
 
 export const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignContent: "center",
+    padding: 20,
+  },
   box1: {
     height: 100,
     width: 100,
@@ -9,4 +15,8 @@ export const style = StyleSheet.create({
   },
   box2: { height: 100, width: 100, backgroundColor: "orange" },
   box3: { height: 100, width: 100, backgroundColor: "yellow" },
+  text: {
+    fontSize: 40,
+    color: "black",
+  },
 });
