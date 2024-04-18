1d54d5bbc8abc4462f70b93088c75071e63402f6
John Huang <pheno_the_best@yahoo.com.tw>
2024-04-19 01:37:48 +0800

    section 28

--- react-native-introduction/App.js
+++ react-native-introduction/App.js
@@ -6,6 +6,7 @@ import {
   TouchableOpacity,
   Alert,
   Linking,
+  Platform,
 } from "react-native";
 import { Profile, ProfileCard } from "./components/ProfileCard/ProfileCard";
 import { AgeCounter } from "./components/AgeCounter/AgeCounter";
@@ -14,32 +15,11 @@ import { style } from "./App.style";
 import { useState } from "react";
 
 export default function App() {
-  function goToSocialMedia(socialMedia) {
-    let url = "";
-    switch (socialMedia) {
-      case "twitter":
-        url = "https://twitter.com/johnhuang_dev";
-        break;
-      case "linkedin":
-        url = "https://www.linkedin.com/in/johnhuangdev/";
-        break;
-      case "github":
-        url = "https://github.com/phenogit";
-      default:
-        break;
-    }
-    Linking.openURL(url);
-  }
-
   return (
     <SafeAreaProvider>
       <SafeAreaView style={style.container}>
-        <ProfileCard
-          onPressSocialMediaIcon={goToSocialMedia}
-          firstName={"John"}
-          lastName={"Huang"}
-          age={42}
-        />
+        {Platform.OS === "android" && <Text>Hi on Android</Text>}
+        {Platform.OS === "ios" && <Text>Hi on iOS</Text>}
       </SafeAreaView>
     </SafeAreaProvider>
   );
