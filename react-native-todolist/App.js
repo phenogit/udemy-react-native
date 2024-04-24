ddbf9469c20ee1f517d7e4ca9a1d40c2272821dd
John Huang <pheno_the_best@yahoo.com.tw>
2024-04-25 01:13:53 +0800

    section 39

--- react-native-todolist/App.js
+++ react-native-todolist/App.js
@@ -1,11 +1,20 @@
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { Text, View } from "react-native";
+import { styles } from "./App.style";
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView>
-        <Text>Todo List!</Text>
+      <SafeAreaView style={styles.app}>
+        <View style={styles.header}>
+          <Text>TODO: Header</Text>
+        </View>
+        <View style={styles.body}>
+          <Text>Body</Text>
+        </View>
+        <View style={styles.footer}>
+          <Text>Footer</Text>
+        </View>
       </SafeAreaView>
     </SafeAreaProvider>
   );
--- react-native-todolist/App.style.js
+++ react-native-todolist/App.style.js
@@ -1,3 +1,26 @@
 import { StyleSheet } from "react-native";
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  app: {
+    flex: 1,
+    backgroundColor: "#F9F9F9",
+  },
+  header: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#F9F9F9",
+    borderBottomColor: "#D9D9D9",
+    borderBottomWidth: 1,
+  },
+  body: {
+    flex: 5,
+    padding: 20,
+  },
+  footer: {
+    height: 70,
+    padding: 20,
+    backgroundColor: "#F9F9F9",
+    borderTopColor: "#D9D9D9",
+    borderTopWidth: 1,
+  },
+});
