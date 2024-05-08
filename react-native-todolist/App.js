372c568dc47b91dc25e2f762b72b2e45493d6205
John Huang <pheno_the_best@yahoo.com.tw>
2024-05-09 00:56:44 +0800

    section 53

--- react-native-todolist/App.js
+++ react-native-todolist/App.js
@@ -5,9 +5,10 @@ import { Header } from "./components/Header/Header";
 import { TodoCard } from "./components/TodoCard/TodoCard";
 import { BottomTab } from "./components/BottomTab/BottomTab";
 import { AddButton } from "./components/AddButton/AddButton";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Dialog from "react-native-dialog";
 import uuid from "react-native-uuid";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 /*
 const TODO_LIST = [
@@ -24,11 +25,51 @@ const TODO_LIST = [
 */
 const TODO_LIST = [];
 
+let isFirstRender = true;
+let isLoadUpdate = false;
+
 export default function App() {
   const [todoList, setTodoList] = useState(TODO_LIST);
   const [selectedTabName, setSelectedTabName] = useState("inProgress");
   const [isAddTodoDialogVisible, setIsAddTodoDialogVisible] = useState(false);
   const [inputValue, setInputValue] = useState("");
+
+  useEffect(() => {
+    loadTodoList();
+  }, []);
+
+  useEffect(() => {
+    if (!isLoadUpdate) {
+      if (!isFirstRender) {
+        saveTodoList();
+      } else {
+        isFirstRender = false;
+      }
+    } else {
+      isLoadUpdate = false;
+    }
+  }, [todoList]);
+
+  async function loadTodoList() {
+    console.log("Loading todo list...");
+    try {
+      const todoListString = await AsyncStorage.getItem("todoList");
+      const parsedTodoList = JSON.parse(todoListString);
+      isLoadUpdate = true;
+      setTodoList(parsedTodoList || []);
+    } catch (error) {
+      alert(error);
+    }
+  }
+
+  async function saveTodoList() {
+    console.log("Saving todo list...");
+    try {
+      await AsyncStorage.setItem("todoList", JSON.stringify(todoList));
+    } catch (error) {
+      alert(error);
+    }
+  }
 
   function getFilteredTodoList() {
     switch (selectedTabName) {
