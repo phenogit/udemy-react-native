dfa5a7cc9a7a6025ba133ab42708c3f099a27f1a
john.huang <pheno_the_best@yahoo.com.tw>
2024-08-05 19:54:50 +0800

    section 58

--- meteo-blank/App.js
+++ meteo-blank/App.js
@@ -1,6 +1,22 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { ImageBackground } from "react-native";
+
+import { s } from "./App.style";
+import { Home } from "./pages/Home/Home";
+import backgroundImg from "./assets/background.png";
 
 export default function App() {
-  return <></>;
+  return (
+    <ImageBackground
+      imageStyle={s.img}
+      style={s.img_background}
+      source={backgroundImg}
+    >
+      <SafeAreaProvider>
+        <SafeAreaView style={s.container}>
+          <Home />
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </ImageBackground>
+  );
 }
---
+++ meteo-blank/pages/Home/Home.jsx
@@ -0,0 +1,18 @@
+import { Text, View } from "react-native";
+import { s } from "./Home.style";
+
+export function Home() {
+  return (
+    <>
+      <View style={s.meteo_basic}>
+        <Text style={s.txt}>Basic Weather Info</Text>
+      </View>
+      <View style={s.searchbar_container}>
+        <Text style={s.txt}>SearchBar</Text>
+      </View>
+      <View style={s.meteo_advanced}>
+        <Text style={s.txt}>Advanced Weather Info</Text>
+      </View>
+    </>
+  );
+}
