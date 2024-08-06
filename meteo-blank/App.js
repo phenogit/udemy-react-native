ccba3d2129c3888dba1c0910b9c8e4d5273c1858
John Huang <pheno_the_best@yahoo.com.tw>
2024-08-07 00:24:11 +0800

    expo-location

--- meteo-blank/App.js
+++ meteo-blank/App.js
@@ -4,8 +4,11 @@ import { ImageBackground } from "react-native";
 import { s } from "./App.style";
 import { Home } from "./pages/Home/Home";
 import backgroundImg from "./assets/background.png";
+import { useEffect } from "react";
 
 export default function App() {
+  useEffect(() => {}, []);
+
   return (
     <ImageBackground
       imageStyle={s.img}
