9476a52b6e902f7b5830d591734fe6e2140b3e54
john.huang <pheno_the_best@yahoo.com.tw>
2024-08-20 22:36:36 +0800

    section 71

--- meteo-blank/components/Txt/Txt.jsx
+++ meteo-blank/components/Txt/Txt.jsx
@@ -1,9 +1,25 @@
-import { Text } from "react-native";
+import { Text, useWindowDimensions } from "react-native";
 import { s } from "./Txt.style";
 
+const SAMSUNG_FOLD_RATIO = 0.0012107933579335792;
+
 export function Txt({ children, style, ...restProps }) {
+  const fontSize = style?.fontSize || s.txt.fontSize;
+  const { height } = useWindowDimensions();
+  console.log("height", height);
+  console.log(Math.round(fontSize * 0.0012107933579335792 * height));
+
   return (
-    <Text style={[s.txt, style]} {...restProps}>
+    <Text
+      style={[
+        s.txt,
+        style,
+        {
+          fontSize: Math.round(fontSize * SAMSUNG_FOLD_RATIO * height),
+        },
+      ]}
+      {...restProps}
+    >
       {children}
     </Text>
   );
--- meteo-blank/components/Txt/Txt.style.js
+++ meteo-blank/components/Txt/Txt.style.js
@@ -2,7 +2,7 @@ import { StyleSheet } from "react-native";
 
 export const s = StyleSheet.create({
   txt: {
-    fontSize: 30,
+    fontSize: 20,
     color: "white",
     fontFamily: "Alata-Regular",
   },
