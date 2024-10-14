49c67dd0910bae4bbe0baafe81bdf07741c8e129
john.huang <pheno_the_best@yahoo.com.tw>
2024-10-14 18:24:57 +0800

    section 83

--- meteo-blank/pages/Forecasts/Forecasts.jsx
+++ meteo-blank/pages/Forecasts/Forecasts.jsx
@@ -1,57 +1,43 @@
 import { s } from "./Forecasts.style";
 import { Txt } from "../../components/Txt/Txt";
 import { useRoute } from "@react-navigation/native";
+import { View } from "react-native";
 import { Header } from "../../components/Header/Header";
 import { ForecastListItem } from "../../components/ForecastListItem/ForecastListItem";
+import { getWeatherInterpretation, DAYS } from "../../utils/meteo-utils";
 // import cloudImg from
 
 export function Forecasts({}) {
   const { params } = useRoute();
+
+  const forecastList = (
+    <View style={{ marginTop: 50 }}>
+      {params.time.map((time, index) => {
+        const weatherCode = params.weathercode[index];
+        const image = getWeatherInterpretation(weatherCode).image;
+        const temperature = params.temperature_2m_max[index];
+        const date = new Date(time);
+        const dayOfTheWeek = DAYS[date.getDay()];
+        const formattedDate = date.toLocaleDateString("default", {
+          day: "numeric",
+          month: "numeric",
+        });
+        return (
+          <ForecastListItem
+            key={time}
+            image={image}
+            day={dayOfTheWeek}
+            date={formattedDate}
+            temperature={temperature.toFixed(0)}
+          />
+        );
+      })}
+    </View>
+  );
   return (
     <>
       <Header city={params.city} />
-      <ForecastListItem
-        image={require("../../assets/clouds.png")}
-        day={"MON"}
-        date={"03/11"}
-        temperature={3}
-      />
-      <ForecastListItem
-        image={require("../../assets/clouds.png")}
-        day={"MON"}
-        date={"03/11"}
-        temperature={3}
-      />
-      <ForecastListItem
-        image={require("../../assets/clouds.png")}
-        day={"MON"}
-        date={"03/11"}
-        temperature={3}
-      />
-      <ForecastListItem
-        image={require("../../assets/clouds.png")}
-        day={"MON"}
-        date={"03/11"}
-        temperature={3}
-      />
-      <ForecastListItem
-        image={require("../../assets/clouds.png")}
-        day={"MON"}
-        date={"03/11"}
-        temperature={3}
-      />
-      <ForecastListItem
-        image={require("../../assets/clouds.png")}
-        day={"MON"}
-        date={"03/11"}
-        temperature={3}
-      />
-      <ForecastListItem
-        image={require("../../assets/clouds.png")}
-        day={"MON"}
-        date={"03/11"}
-        temperature={32}
-      />
+      {forecastList}
     </>
   );
 }
