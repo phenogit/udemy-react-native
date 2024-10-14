9b6f54b36dd219bcfcedf1b67b1920fa43a9f4d6
john.huang <pheno_the_best@yahoo.com.tw>
2024-10-14 17:46:57 +0800

    section 82

--- meteo-blank/pages/Forecasts/Forecasts.jsx
+++ meteo-blank/pages/Forecasts/Forecasts.jsx
@@ -2,12 +2,56 @@ import { s } from "./Forecasts.style";
 import { Txt } from "../../components/Txt/Txt";
 import { useRoute } from "@react-navigation/native";
 import { Header } from "../../components/Header/Header";
+import { ForecastListItem } from "../../components/ForecastListItem/ForecastListItem";
+// import cloudImg from
 
 export function Forecasts({}) {
   const { params } = useRoute();
   return (
     <>
-      <Header city={params.city}></Header>
+      <Header city={params.city} />
+      <ForecastListItem
+        image={require("../../assets/clouds.png")}
+        day={"MON"}
+        date={"03/11"}
+        temperature={3}
+      />
+      <ForecastListItem
+        image={require("../../assets/clouds.png")}
+        day={"MON"}
+        date={"03/11"}
+        temperature={3}
+      />
+      <ForecastListItem
+        image={require("../../assets/clouds.png")}
+        day={"MON"}
+        date={"03/11"}
+        temperature={3}
+      />
+      <ForecastListItem
+        image={require("../../assets/clouds.png")}
+        day={"MON"}
+        date={"03/11"}
+        temperature={3}
+      />
+      <ForecastListItem
+        image={require("../../assets/clouds.png")}
+        day={"MON"}
+        date={"03/11"}
+        temperature={3}
+      />
+      <ForecastListItem
+        image={require("../../assets/clouds.png")}
+        day={"MON"}
+        date={"03/11"}
+        temperature={3}
+      />
+      <ForecastListItem
+        image={require("../../assets/clouds.png")}
+        day={"MON"}
+        date={"03/11"}
+        temperature={32}
+      />
     </>
   );
 }
